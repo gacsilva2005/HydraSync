ec7bd7bbc69ae634aacf79670998e595e0e0abe9
Gabriel Silva <donnerff076@gmail.com>
2026-03-30 16:11:25 -0300

    Feat: Adicionando estrutura de pastas para o Spring Boot.
    Dependências adicionadas:
    - Spring Boot Dev Tools;
    - Spring Boot Web;
    - LomBok.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/ScHydraSenseApplication.java
@@ -0,0 +1,13 @@
+package com.hydrasense.schydrasense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScHydraSenseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScHydraSenseApplication.class, args);
+    }
+
+}
