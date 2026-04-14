dcb51c34055ce71cafb174e9ea24d90bf05178a9
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-14 11:20:27 -0300

    Feature: Criação da classe "Profissional", seguindo a documentação e diagramas.
    - Classes Controller, Repository, Service e Model criadas;
    - Caminho para a API CorsConig em \config.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/repository/ProfissionalRepository.java
@@ -0,0 +1,7 @@
+package com.hydrasense.schydrasense.repository;
+
+import com.hydrasense.schydrasense.model.Profissional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+}
