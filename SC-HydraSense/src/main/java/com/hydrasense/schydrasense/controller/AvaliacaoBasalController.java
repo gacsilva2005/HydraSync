2fde24e6aa88048d20193159bba1747e70700631
Sergio Oliveira <sholiveira.sho@gmail.com>
2026-04-16 16:06:06 -0300

    refactor: removendo-cross-origin

--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/controller/AvaliacaoBasalController.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/controller/AvaliacaoBasalController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/avaliacao-basal")
 public class AvaliacaoBasalController {
--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/controller/CondicaoAmbientalController.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/controller/CondicaoAmbientalController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/condicao-ambiental")
 public class CondicaoAmbientalController {
