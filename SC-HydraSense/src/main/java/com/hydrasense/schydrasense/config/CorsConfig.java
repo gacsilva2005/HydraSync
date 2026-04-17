307cd6d0f21ea0720c2006976075183045d34699
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-17 10:20:48 -0300

    Feature: Configurando entidade Atleta.
    - Configuração de métodos do CorsConfig.java;
    - Configuração da entidade para ser consumida no React.

--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/config/CorsConfig.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/config/CorsConfig.java
@@ -13,8 +13,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") //Só exemplo de uma porta por enquanto
-                        .allowedMethods("*");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
             }
         };
     }
