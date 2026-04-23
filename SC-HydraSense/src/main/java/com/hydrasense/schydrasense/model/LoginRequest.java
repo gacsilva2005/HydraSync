be6c08b8bad846128e23e7786ad9451888823188
Gabiel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-23 10:01:03 -0300

    Feature: Adiciona nova variável para recebimento de informações em LoginRequest.

--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/model/LoginRequest.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/model/LoginRequest.java
@@ -2,12 +2,15 @@ package com.hydrasense.schydrasense.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LoginRequest {
 
     private String email;
     private String senha;
+    private String tipoUsuario;
 
 }
