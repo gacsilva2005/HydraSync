8760b454e827dccdee08e34a7dc3822c58928b21
Gabiel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-23 09:28:48 -0300

    Feature: Classe LoginRequest criada.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/model/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.hydrasense.schydrasense.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    private String email;
+    private String senha;
+
+}
