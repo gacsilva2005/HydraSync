01e66ba8169f87066548eff444fbddaac839d841
Gabiel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-23 09:59:47 -0300

    Feature: Cria método de busca em AtletaRepository.
    - Método para busca de email do Atleta.

--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/repository/AtletaRepository.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/repository/AtletaRepository.java
@@ -1,7 +1,10 @@
 package com.hydrasense.schydrasense.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.hydrasense.schydrasense.model.Atleta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface AtletaRepository extends JpaRepository<Atleta, Long> {
+    Optional<Atleta> findByEmail(String email);
 }
