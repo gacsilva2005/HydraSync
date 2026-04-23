3e4f840a0cffec8f2eef0b3ad5516c2f4fbb0b52
Gabiel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-23 09:13:09 -0300

    Feature: Adiciona método no repository de Profissional.
    - Procura email e senha do Profissional de acordo com os parâmetros passados.

--- SC-HydraSense/src/main/java/com/hydrasense/schydrasense/repository/ProfissionalRepository.java
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/repository/ProfissionalRepository.java
@@ -3,5 +3,8 @@ package com.hydrasense.schydrasense.repository;
 import com.hydrasense.schydrasense.model.Profissional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+    Optional<Profissional> findByEmailAndSenha(String email, String senha);
 }
