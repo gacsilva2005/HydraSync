a890f68bdeaa9943460e9b3b983923dfefb2245b
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-04-28 16:57:05 -0300

    Feature: integração de login com Google OAuth2 e proteção de rotas
    - configuração do Spring Security OAuth2
    - criação do endpoint /auth/user
    - implementação do login com Google no frontend
    - criação da página PageWeb com carregamento do usuário autenticado
    - ajuste das rotas com React Router

--- SC-HydraSense/Front-End/sc-hydrasense-web/src/App.tsx
+++ SC-HydraSense/Front-End/sc-hydrasense-web/src/App.tsx
@@ -1,36 +1,38 @@
-import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
-import { Register } from './pages/Login/Register';
-import { Identifier } from './pages/Login/Identifier';
-import { Team } from './pages/Login/Team';
-import { Athlete } from './pages/Login/Athlete';
-import { PageWeb } from './pages/PaginasSite/PageWeb';
-import { Atletas } from './pages/PaginasSite/Atletas';
-import { Dashboard } from './pages/PaginasSite/Dashboard';
-import { Equipes } from './pages/PaginasSite/Equipes';
-import { Relatorios } from './pages/PaginasSite/Relatorios';
-import { Configuracoes } from './pages/PaginasSite/Configuracoes';
+import { Register } from "./pages/Login/Register";
+import { Identifier } from "./pages/Login/Identifier";
+import { Team } from "./pages/Login/Team";
+import { Athlete } from "./pages/Login/Athlete";
+
+import { PageWeb } from "./pages/PaginasSite/PageWeb";
+import { Atletas } from "./pages/PaginasSite/Atletas";
+import { Dashboard } from "./pages/PaginasSite/Dashboard";
+import { Equipes } from "./pages/PaginasSite/Equipes";
+import { Relatorios } from "./pages/PaginasSite/Relatorios";
+import { Configuracoes } from "./pages/PaginasSite/Configuracoes";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/registro" element={<Register />} />
-      <Route path="/identificador" element={<Identifier />} />
-      <Route path="/equipe" element={<Team />} />
-      <Route path="/athlete" element={<Athlete />} />
+    return (
+        <Routes>
+            {/* Rotas públicas */}
+            <Route path="/" element={<Home />} />
+            <Route path="/registro" element={<Register />} />
+            <Route path="/identificador" element={<Identifier />} />
+            <Route path="/equipe" element={<Team />} />
+            <Route path="/athlete" element={<Athlete />} />
 
-      {/* Suas rotas do Painel Web */}
-      <Route path="/PageWeb" element={<PageWeb />}>
-        <Route index element={<Dashboard />} />
-        <Route path="atletas" element={<Atletas />} />
-        <Route path="equipes" element={<Equipes />} />
-        <Route path="relatorios" element={<Relatorios />} />
-        <Route path="configuracoes" element={<Configuracoes />} />
-      </Route>
-    </Routes>
-  );
+            {/* Rotas do Painel Web */}
+            <Route path="/PageWeb" element={<PageWeb />}>
+                <Route index element={<Dashboard />} />
+                <Route path="atletas" element={<Atletas />} />
+                <Route path="equipes" element={<Equipes />} />
+                <Route path="relatorios" element={<Relatorios />} />
+                <Route path="configuracoes" element={<Configuracoes />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
